163ed4411fa5d4070b8710189622f88dda033a8c
malloryrbush <malloryrbush@hotmail.com>
2018-07-15 14:06:57 -0400

    first commit

---
+++ TravelReviewSite/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        public string City { get; set; }
+        public string Attractions { get; set; }
+        public string Hotels { get; set; }
+
+        public virtual ICollection<Review> Review { get; set; }
+    }
+}
---
+++ TravelReviewSite/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string PublishDate { get; set; }
+        public string Author { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
